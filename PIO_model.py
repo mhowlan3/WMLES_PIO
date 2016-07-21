ebe33582c5a8da58f1eeece05264782160f707d8
Mike <mike.howland13@gmail.com>
2016-07-21 11:24:12 -0700

    update

--- PIO_model.py
+++ PIO_model.py
@@ -15,10 +15,16 @@ import struct
 # Parameters
 retau = 2000
 plane_num = 1
-d=2.5
+d=3.5
 folder = ''
 folder = 'data/' + 'Retau' + str(retau) + '/' + 'plane_' + str(plane_num) + '/' + str(d) + 'd/'
 #print folder
+
+# Some operation functions
+def rms(val):
+	rms_val = np.sqrt(np.mean(np.square(val)))
+	return rms_val
+	
 
 # Load calibration data
 nt = 10
@@ -42,12 +48,11 @@ for t in range(1, nt+1):
 			tplus = data[0 : nx - 1]
 		else:
 			tau_w_star[:,j-1] = data[(j)*nx : (j+1)*nx]
-			tau_rms[j-1] = np.sqrt(np.mean(np.square(tau_w_star[:,j-1])))
+			tau_rms[j-1] = rms(data[(j)*nx : (j+1)*nx])
+			#tau_rms[j-1] = np.sqrt(np.mean(np.square(tau_w_star[:,j-1])))
 	print "Time step plane = %s" % t
 	print "Average tau_rms = %s" % np.mean(tau_rms)
 	print "STD tau_rms = %s" % np.std(tau_rms)
-
-
 
 
 
